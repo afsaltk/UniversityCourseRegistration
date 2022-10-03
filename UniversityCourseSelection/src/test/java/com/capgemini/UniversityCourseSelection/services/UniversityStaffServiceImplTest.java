0d6e6f0fd6b0732cd5efd1ade3d0e5e7baceed79
askavin200 <114135375+askavin200@users.noreply.github.com>
2022-10-03 10:59:35 +0530

    Update UniversityStaffServiceImplTest.java

--- UniversityCourseSelection/src/test/java/com/capgemini/UniversityCourseSelection/services/UniversityStaffServiceImplTest.java
+++ UniversityCourseSelection/src/test/java/com/capgemini/UniversityCourseSelection/services/UniversityStaffServiceImplTest.java
@@ -30,15 +30,15 @@ class UniversityStaffServiceImplTest {
 	private UniversityStaffServiceImpl staffService;
 	
 	
-	UniversityStaffMember STAFF_1 = new UniversityStaffMember(1,"password1","role1");
-	UniversityStaffMember STAFF_2 = new UniversityStaffMember(2,"password2","role2");
-	UniversityStaffMember STAFF_3 = new UniversityStaffMember(3,"password3","role3");
+	UniversityStaffMember STAFF_1 = new UniversityStaffMember(1,"staff1","password1","role1");
+	UniversityStaffMember STAFF_2 = new UniversityStaffMember(2,"staff2","password2","role2");
+	UniversityStaffMember STAFF_3 = new UniversityStaffMember(3,"staff3","password3","role3");
 	
 	Course COURSE_1 = new Course(1,"Java Programming","2 months",LocalDate.of(2022, 6, 25),LocalDate.of(2022, 8, 25),"700",96.5);
 	
 	@Test
 	void addStaff_success() {
-		UniversityStaffMember newStaff = new UniversityStaffMember(4,"password4","role4");
+		UniversityStaffMember newStaff = new UniversityStaffMember(4,"staff4","password4","role4");
 		Mockito.when(staffRepo.save(newStaff)).thenReturn(newStaff);
 		assertEquals(4,staffService.addStaff(newStaff).getStaffId());
 		assertEquals("password4",staffService.addStaff(newStaff).getPassword());
@@ -48,7 +48,7 @@ class UniversityStaffServiceImplTest {
 
 	@Test
 	void updateStaff_success() {
-		UniversityStaffMember updateStaff = new UniversityStaffMember(1,"new_pwd","new_role");
+		UniversityStaffMember updateStaff = new UniversityStaffMember(1,"new_staff","new_pwd","new_role");
 		Mockito.when(staffRepo.existsById(updateStaff.getStaffId())).thenReturn(true);
 		Mockito.when(staffRepo.save(updateStaff)).thenReturn(updateStaff);
 		assertEquals("new_pwd",staffService.updateStaff(updateStaff).getPassword());
